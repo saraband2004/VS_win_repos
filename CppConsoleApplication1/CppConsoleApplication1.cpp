b84c83e4a5813a960e21556e3864ec7bed8ab3c7
saraband2004 <53579138+saraband2004@users.noreply.github.com>
2020-05-03 23:50:45 -0400

    5/3

--- CppConsoleApplication1/CppConsoleApplication1.cpp
+++ CppConsoleApplication1/CppConsoleApplication1.cpp
@@ -3,62 +3,62 @@
 #include <vector>
 #include <set>
 #include <unordered_set>
-
+#include <stack>
+#include <algorithm>
+#include <array>
 using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> Stack;
+        heights.push_back(0);
+        int max = 0;
+        int min = 1 << 30 + 1<<29;
 
+        vector<int> arr;
+        for (int i = 0; i < heights.size(); i++) {
+            arr.push_back(heights[i]);
+        }
+        
+        for (int i = 0; i < heights.size(); ) {
+            if (Stack.empty() || heights[i] > arr[Stack.top()]) {
+                Stack.push(i);
+                min = std::min(min, heights[i]);
+                i++;
+            }
+            else if (heights[i] == arr[Stack.top()]) {
+                max = std::max(max, heights[i] * (i - Stack.top() + 1));
+                i++;
+            }
+            else {
+                auto j = Stack.top();
+                while (!Stack.empty() && arr[Stack.top()] > heights[i]) {
+                    j = Stack.top();
+                    Stack.pop();
+                }
+                
+                max = std::max(max, arr[j] * (i - j));
+                arr[j] = heights[i];
+                max = std::max(max, arr[j] * (i - j + 1));
+                Stack.push(j);
+            }
+        }
+        
+        return max; //std::max(max, (signed int)(min * heights.size()));
     }
 };
- 
-void print_vector(vector<int>& list) {
-    for (int i = 0; i < list.size(); i++) {
-        cout << list.at(i) << " ";
-    }
-    cout << " " << endl;
-}
 
 
 int main(int argc, const char* argv[]) {
-    /*
-    vector<int> list;
-    list.push_back(1);
-    list.push_back(22);
-    list.push_back(333);
-    list.push_back(4444);
-    list.push_back(55555);
-    list.push_back(666666);
-    list.push_back(77777);
-    print_vector(list);
-    cout << list.capacity() << endl; 
-    
-    list.erase(list.begin() + 1);
+    Solution s;
+    vector<int> h;
+    h.push_back(0);
+    h.push_back(3);
+    h.push_back(2);
+    h.push_back(5);
+    cout << s.largestRectangleArea(h);
 
-    print_vector(list);
-
-    list.insert(list.end() - 1, 999);
-    
-    print_vector(list);
-
-    vector<int>::iterator it = list.end();
-
-    cout << *(it-1) << endl;
-    */
-
-
-    unordered_set<int> hset;
-    hset.insert(1);
-    hset.insert(-22);
-    hset.insert(333);
-    hset.insert(-444);
-    hset.insert(1);
-    
-    if (hset.find(324) == hset.end()) {
-        cout << "OKAY" << endl;
-    }
-    
     return 0;
 
 }
